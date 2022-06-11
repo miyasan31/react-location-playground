95eb2c8dfd07a7ebbc0b5d14bff36f8ba9a93447
miyasan <71614432+miyasan31@users.noreply.github.com>
2022-06-11 16:10:43 +0900

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import "~/styles/index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import AppProvider from "~/providers";
+import { AppRoutes } from "~/routes";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppProvider>
+      <AppRoutes />
+    </AppProvider>
+  </React.StrictMode>,
+);
